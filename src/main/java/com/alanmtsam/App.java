bf7bc493b987edfd2c5ddab971e7dd6e5b701b6f
alanmts-am <alanmts.am@gmail.com>
2026-05-13 21:12:52 -0300

    feat: initial commit

---
+++ src/main/java/com/alanmtsam/App.java
@@ -0,0 +1,13 @@
+package com.alanmtsam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
